60f6d941e63fad6f2e28f18566546389d11afe01
Dawood khokhar <khokhardawood59@Gmail.com>
2024-12-19 20:58:55 +0500

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,16 +1,13 @@
-import { useContext } from 'react';
-import { AppContext } from '../context/Productcontext';
+import react from 'react';
+
 import Footer from './Footer'; // Corrected the folder name
 
 const About = () => {
-  const myName = useContext(AppContext);
-  const Data = {
-    name: "Dawood store"
-  };
+
 
   return (
     <div>
-      {myName}
+      
       <img
         src="https://i.ytimg.com/vi/nz4Zqy8uyEY/maxresdefault.jpg"
         width={1400}
